1e7a2052bf26aa08bb98ac8162a378928e50a068
Jepoy Factoran <factoran20@gmail.com>
2022-05-15 14:14:22 +0800

    add description on API route methods

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,15 +1,24 @@
+// @desc Get all goals
+// @route GET /api/goals
+// @access PRIVATE
 const getGoals = (req, res) => {
   res.status(200).json({ message: "Git Goals" });
 };
-
+// @desc Set a goal
+// @route POST /api/goals
+// @access PRIVATE
 const setGoal = (req, res) => {
   res.status(200).json({ message: "Set Goal" });
 };
-
+// @desc Update a goal
+// @route UPDATE /api/goals/:id
+// @access PRIVATE
 const updateGoal = (req, res) => {
   res.status(200).json({ message: `Update Goal ${req.params.id}` });
 };
-
+// @desc Delete a goal
+// @route DELETE /api/goals/:id
+// @access PRIVATE
 const deleteGoal = (req, res) => {
   res.status(200).json({ message: `Delete Goal ${req.params.id}` });
 };
